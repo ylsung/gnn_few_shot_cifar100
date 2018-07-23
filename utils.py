2f5898bb50254aa2ebb31aa5d0fd83cdb6b4a3c0
yilin sung <r06942076@ntu.edu.tw>
2018-07-23 19:26:20 +0800

    delete redundant class in utils.py

--- utils.py
+++ utils.py
@@ -26,27 +26,3 @@ def create_logger(save_path='', file_type=''):
 def mkdir(path):
     if not os.path.exists(path):
         os.mkdir(path)
-
-class self_Dataset(Dataset):
-    def __init__(self, data, label=None, transform=None):
-        super(self_Dataset, self).__init__()
-
-        self.data = data
-        self.label = label
-        self.transform = transform
-    def __getitem__(self, index):
-        data = self.data[index]
-        # data = np.moveaxis(data, 3, 1)
-        # data = data.astype(np.float32)
-
-        data = self.transform(data)
-        if self.label is not None:
-            label = self.label[index]
-            # print(label)
-            # label = torch.from_numpy(label)
-            # label = torch.LongTensor([label])
-            return data, label
-        else:
-            return data, 1
-    def __len__(self):
-        return len(self.data)
